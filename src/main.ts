f823e6b1521d6eb0b5aaf5fc1baca84917a61ea0
koba1108 <paseli.koba@gmail.com>
2020-05-15 16:09:03 +0900

    add

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import {NestFactory} from '@nestjs/core';
+import {AppModule} from './app.module';
+
+const PORT = process.env.PORT || 3000
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.setGlobalPrefix('api')
+  await app.listen(PORT);
 }
-bootstrap();
+
+bootstrap().then(() => {
+  console.log(`start listen port ${PORT}`)
+}).catch(e => {
+  console.error(e)
+});
